ede457414a5e4db59c297bd60175e7e36982d47c
Richard Fenix <richard.fenix@gmail.com>
2018-06-21 14:57:58 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -17,37 +17,37 @@ class Transfer
   
 end
 
-  describe '#valid?' do
-      it "can check that both accounts are valid" do
-        expect(avi.valid?).to eq (true)
-        expect(amanda.valid?).to eq(true)
-        expect(transfer.valid?).to eq(true)
-      end
+  # describe '#valid?' do
+  #     it "can check that both accounts are valid" do
+  #       expect(avi.valid?).to eq (true)
+  #       expect(amanda.valid?).to eq(true)
+  #       expect(transfer.valid?).to eq(true)
+  #     end
 
-      it "calls on the sender and reciever's #valid? methods" do
-        transfer_class = File.read("lib/transfer.rb")
+  #     it "calls on the sender and reciever's #valid? methods" do
+  #       transfer_class = File.read("lib/transfer.rb")
 
-        expect(amanda).to receive(:valid?).and_return(true)
-        expect(avi).to receive(:valid?).and_return(true)
+  #       expect(amanda).to receive(:valid?).and_return(true)
+  #       expect(avi).to receive(:valid?).and_return(true)
 
-        transfer.valid?
-      end
-    end
+  #       transfer.valid?
+  #     end
+  #   end
 
-    describe '#execute_transaction' do
-      it "can execute a successful transaction between two accounts" do
-        transfer.execute_transaction
-        expect(amanda.balance).to eq(950)
-        expect(avi.balance).to eq(1050)
-        expect(transfer.status).to eq("complete")
-      end
+  #   describe '#execute_transaction' do
+  #     it "can execute a successful transaction between two accounts" do
+  #       transfer.execute_transaction
+  #       expect(amanda.balance).to eq(950)
+  #       expect(avi.balance).to eq(1050)
+  #       expect(transfer.status).to eq("complete")
+  #     end
 
-      it "each transfer can only happen once" do
-        transfer.execute_transaction
-        expect(amanda.balance).to eq(950)
-        expect(avi.balance).to eq(1050)
-        expect(transfer.status).to eq("complete")
-        transfer.execute_transaction
-        expect(amanda.balance).to eq(950)
-        expect(avi.balance).to eq(1050)
-      end
+  #     it "each transfer can only happen once" do
+  #       transfer.execute_transaction
+  #       expect(amanda.balance).to eq(950)
+  #       expect(avi.balance).to eq(1050)
+  #       expect(transfer.status).to eq("complete")
+  #       transfer.execute_transaction
+  #       expect(amanda.balance).to eq(950)
+  #       expect(avi.balance).to eq(1050)
+  #     end
