e994d86fb1421315f515656ee263f7eca1d84689
Richard Fenix <richard.fenix@gmail.com>
2018-06-21 14:35:54 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -30,24 +30,4 @@ class BankAccount
 
 end
 
-# describe '#close_account' do
-#     it "can close its account" do
-#       avi.close_account
-#       expect(avi.status).to eq("closed")
-#     end
-#   end
-# end
-
-# describe '#valid?' do
-#     it "is valid with an open status and a balance greater than 0" do
-#       @broke = BankAccount.new("Kat Dennings")
-#       @broke.balance = 0
-#       @closed = BankAccount.new("Beth Behrs")
-#       @closed.status = "closed"
-#       expect(avi.valid?).to eq(true)
-#       expect(@broke.valid?).to eq(false)
-#       expect(@closed.valid?).to eq(false)
-#     end
-#   end
-
  
