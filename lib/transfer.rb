730e055a928ee95da076fcbbf0b259ec122a4800
Richard Fenix <richard.fenix@gmail.com>
2018-06-21 14:44:37 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -11,15 +11,6 @@ class Transfer
   
   
   
-      describe 'initialize' do
 
-      it "always initializes with a status of 'pending'" do
-        expect(transfer.status).to eq("pending")
-      end
-
-      it "initializes with a transfer amount" do
-        expect(transfer.amount).to eq(50)
-      end
-  
   
 end
