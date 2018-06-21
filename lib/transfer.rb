b430846242e91b67d82e5549e19ed58357f3403e
Richard Fenix <richard.fenix@gmail.com>
2018-06-21 14:40:04 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,13 +1,32 @@
 class Transfer
 
-  attr_accessor :from, :to, :amount
+  attr_accessor :sender, :receiver, :amount
   
-  def initialize(from,to,amount)
-    @from = from
-    @to = to
+  def initialize(sender,receiver,amount)
+    @sender = sender
+    @receiver = receiver
     @amount = amount
   end
   
+  
+  
+      describe 'initialize' do
+
+      it "initializes with a sender" do
+        expect(transfer.sender).to eq(amanda)
+      end
+
+      it "initializes with a receiver" do
+        expect(transfer.receiver).to eq(avi)
+      end
+
+      it "always initializes with a status of 'pending'" do
+        expect(transfer.status).to eq("pending")
+      end
+
+      it "initializes with a transfer amount" do
+        expect(transfer.amount).to eq(50)
+      end
   
   
 end
