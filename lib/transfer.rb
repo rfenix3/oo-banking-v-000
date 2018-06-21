e69bb9de96077576fa2345a23e79dcb477e6961f
Richard Fenix <richard.fenix@gmail.com>
2018-06-21 14:51:00 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,6 +1,6 @@
 class Transfer
 
-  attr_accessor :sender, :receiver, :amount
+  attr_accessor :sender, :receiver, :amount, :statis
   
   def initialize(sender,receiver,amount)
     @sender = sender
